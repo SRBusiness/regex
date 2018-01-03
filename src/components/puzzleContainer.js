838920e3b9b74e888ede584e615a72809154280a
Sarah Read-Brown <srbusiness2@gmail.com>
2018-01-03 11:05:24 -0800

    Figured out mismatched html style elements issue. Now using two overlapping strings

--- src/components/puzzleContainer.js
+++ src/components/puzzleContainer.js
@@ -9,12 +9,12 @@ import {regexHighlight } from './highlight';
 
 const puzzles = {
   0:{
-    fact: `Otters love to eat shelled animals, like clams, but <b class='highlight'>otters</b> aren’t equipped with the strength to open their food without some help. Otters are big on tools, and will often use rocks to help crack into dinner. While they hunt for food underwater, they’ll often store a rock in the skin under their arms for later use.`,
+    fact: `Otters love to eat shelled animals, like clams, but <b>otters</b> aren’t equipped with the strength to open their food without some help. Otters are big on tools, and will often use rocks to help crack into dinner. While they hunt for food underwater, they’ll often store a rock in the skin under their arms for later use.`,
     prompt: 'Build a Regex that matches the word "rock"',
     answer: 'rock',
   },
   1: {
-    fact: `There are <b class='highlight'>13</b> species of Otters, and just about all of them are decreasing.`,
+    fact: `There are <b>13</b> species of Otters, and just about all of them are decreasing.`,
     prompt: 'Build a Regex that matches the number "13"',
     answer: '13',
   }
@@ -34,15 +34,23 @@ class Puzzle extends React.Component {
       <div className='puzzle'>
         {ReactHtmlParser(fact)}
         <p>{this.props.lesson.prompt}</p>
-        <p>
-          <em>O<b>tter</em></b>
 
-        </p>
+        <div className='puzzle-display'>
+          <p className='top'>The Ot<span className='highlight-one'>ters</span> are cool animals!</p>
+          <p className='bottom'>The <span className='highlight-two'>Otte</span>rs are cool animals!</p>
+
+        </div>
       </div>
 
     )
   }
 }
+// 'The <b>Otter</b> are cool animals!'
+// 'The otterman wanted to be an otter'
+// split(/<\/?b>/) => [ 'The ', 'Otter', 's are cool animals!' ]
+// is there a way to do this that isnt inline styling? something do with absolute position by text?
+// two overlapping strings one is underlined one is highlighted
+
 
 Puzzle.propTypes = {
   puzzle: PropTypes.shape({
