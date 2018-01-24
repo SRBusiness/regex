7dc8ef9e81c38c7657112e69fe037f2ef0e41d6b
Sarah Read-Brown <srbusiness2@gmail.com>
2018-01-24 00:14:08 -0800

    I think i was in a sub folder prior which is why git add. was acting up. Added image and favicon

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -38,6 +38,7 @@ const Game = ({ level, refSheetContent, toggleRef, paragraph, contentObj }) => {
       <footer className="footer">
         { toggleRef ? <RefTables refArray={refSheetContent} /> : null }
         <Information/>
+        {/* <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
       </footer>
     </div>
   )
--- src/components/otterFact/otterFact.js
+++ src/components/otterFact/otterFact.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './otterFact.css';
+import otterVector from './otterhead1.png';
 
 // Functional stateless component
 const OtterFact = ({fact}) => {
   return (
-    <div className={styles.container}>
-      <p><span className={styles.fact}>Otter Fact:</span> {fact} </p>
+    <div className={styles.otterFactContainer}>
+      <div>
+        <img src={otterVector} alt="Otter" /><p><span className={styles.fact}>Otter Fact:</span> {fact} </p>
+      </div>
+
+      {/* <figure> */}
+      {/* <div>
+        <img src={otterVector} alt="Otter" />
+      </div> */}
+
+      {/* </figure> */}
+
     </div>
   )
 }
